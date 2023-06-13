c688dc56fe57fd387320a5d5bd2ac57c72d50ed4
lary09 <laryfiguereo@gmail.com>
2023-06-13 13:24:42 -0400

    add entity and context

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
+using APIWine.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using System.Diagnostics.Metrics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer("Server=DESKTOP-I0TRJDL\\SQLEXPRESS01;Database=wine;User ID=sa;Password=12345678;TrustServerCertificate=true;");
+});
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
